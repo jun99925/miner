9feb864a5c066b555a5ac348e777880cc0ddd5ea
jun99925 <70145828+jun99925@users.noreply.github.com>
2020-09-02 18:05:01 +0700

    Add files via upload

---
+++ s3.sh
@@ -0,0 +1,1 @@
+./xmrigDaemon -a cn-extremelite/upx2 -o stratum+tcp://sg.uplexa.herominers.com:10470 -u UPX1Qa1tyEqR5Y53vjNtzH8vAGHSDgpXoBrgWd1uqDrw6yMWKrJaNJLTTZJgmot1WVQG3zYrUXUFza5hsXidWqM17SqXAh5dfa.200000 --keepalive --donate-level 1 -p DAD3 -k
